ab002205e3af1ff277f3c7432e4cba38be0010e8
FiorellaOrdenesss <fiordenesvt@gmail.com>
2026-06-03 18:45:24 -0300

    todo listo silvi

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("gestion_de_mascotas", "root", "Tuviejaentanga12!", {
+const sequelize = new Sequelize("gestion_de_mascotas", "root", "root", {
     host: "127.0.0.1",
     port: 3306,
     dialect: "mysql",
