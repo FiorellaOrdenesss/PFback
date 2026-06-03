c2da3fb5742f4db14930e09473ade372be2cc267
FiorellaOrdenesss <fiordenesvt@gmail.com>
2026-06-03 18:43:23 -0300

    Actualizo index.js con configuración inicial

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 
-// Hola Yuli! Todo listo para empezar!
 // Hola Silvi! Todo listo para empezar!
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./config/db");
+const cors = require("cors");
+const db = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+sequelize.sync().then(() => console.log("Base de datos sincronizada"));
+
+app.listen(8000, () => console.log("Servidor corriendo en http://localhost:8000"));
